908d234e61bc85239dd17b61c082a0e19f2ab037
ALEXGF <alexgf2703@gmail.com>
2019-01-05 17:07:28 -0500

    version 2 Se añadió pasar parametro String de una caja de texto de un activity a otro

--- app/src/main/java/com/example/alexgf/intentapp/MainActivity.java
+++ app/src/main/java/com/example/alexgf/intentapp/MainActivity.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_name);
+
     }
 
     //Metodo boton siguiente
     public void nextActivity(View view){
-        Intent nextActivity = new Intent(this, SecondActivity.class);
-        startActivity(nextActivity);
+        Intent nextActivity = new Intent(this, SecondActivity.class);   //se indica a que activida se quiere ir
+        nextActivity.putExtra("nombre", et1.getText().toString());      //se pasa el parametro
+        startActivity(nextActivity);        //se activa la otra actividad
     }
 }
--- app/src/main/java/com/example/alexgf/intentapp/SecondActivity.java
+++ app/src/main/java/com/example/alexgf/intentapp/SecondActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        tv1 = (TextView)findViewById(R.id.txt1_act2);
+
+        String name = getIntent().getStringExtra("nombre");
+        tv1.setText("Hola " + name);
     }
 
     //método boton anterior
